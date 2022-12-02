04f2a829b68e24d5cfb803c1df598a401a892771
NIKITA LITVINENKO <litvinovnick24@gmail.com>
2022-12-02 17:36:32 +0300

    task3 is completed

--- lesson_1/code_3/Program.cs
+++ lesson_1/code_3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 6: Напишите программу, 
+которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0) {
+    Console.WriteLine("num - четное");
+} else {
+    Console.WriteLine("num - нечетное");
+}
