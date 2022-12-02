b8f52f1824da792066bc9f6c622d186d427c302a
NIKITA LITVINENKO <litvinovnick24@gmail.com>
2022-12-02 17:04:52 +0300

    Çàäà÷à 1 ðåøåíà

--- lesson_1/code_1/Program.cs
+++ lesson_1/code_1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+int max = 0, min = 0;
+
+Console.Write("a = ");
+int numA = int.Parse(Console.ReadLine());
+
+Console.Write("b = ");
+int numB = int.Parse(Console.ReadLine());
+
+
+if (numA > numB) {
+    max = numA;
+    min = numB;
+    //Console.WriteLine("numA > numB");
+} else {
+    max = numB;
+    min = numA;  
+    //Console.WriteLine("numB > numA");
+}
+Console.Write("max = ");
+Console.WriteLine(max);
+// Console.Write("min = ");
+// Console.WriteLine(min);
