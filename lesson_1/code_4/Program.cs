b5a129f5ea692bcd27a9e669abebf9d3da3f8c58
NIKITA LITVINENKO <litvinovnick24@gmail.com>
2022-12-02 17:42:54 +0300

    task4 is completed

--- lesson_1/code_4/Program.cs
+++ lesson_1/code_4/Program.cs
@@ -6,3 +6,12 @@
 5 -> 2, 4
 8 -> 2, 4, 6, 8
 */
+
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < num; i++) {
+    if (i % 2 == 0) {
+        Console.Write(i);
+        if (i < num - 2) Console.Write(", ");
+    }
+}
