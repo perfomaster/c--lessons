ea1e9c07a10fa5616d9789486630cf8902ebe055
NIKITA LITVINENKO <litvinovnick24@gmail.com>
2022-12-03 22:33:51 +0300

    code_3 is completed

--- lesson_4/code_3/Program.cs
+++ lesson_4/code_3/Program.cs
@@ -5,3 +5,25 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
+
+int N = int.Parse(Console.ReadLine());
+
+int[] arr = fillArray(N);
+showArray(arr);
+
+
+int[] fillArray (int n) {
+    int[] array = new int[n];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = random.Next(0, 100);
+    }
+    return array;
+}
+
+void showArray (int[] array) {
+    for (int i = 0; i < array.Length; i++) {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+}
