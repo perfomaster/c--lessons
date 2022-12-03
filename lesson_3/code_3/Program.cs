0cf385d298661a0c0e54ab02bbc981c0b6e93214
NIKITA LITVINENKO <litvinovnick24@gmail.com>
2022-12-03 22:01:03 +0300

    code 3 is completed

--- lesson_3/code_3/Program.cs
+++ lesson_3/code_3/Program.cs
@@ -7,4 +7,20 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
-
+Console.Write("N=");
+int N = int.Parse(Console.ReadLine());
+int[] result = new int[N];
+result = writeCube(N);
+for (int i = 0; i < result.Length; i++) {
+    if (i < result.Length - 1) Console.Write($"{result[i]}, ");
+    else Console.Write(result[i]);
+}
+int[] writeCube(int input) {
+    int[] arr = new int [input];
+    int index = 0;
+    while (index < input) {
+        arr[index] = (int)Math.Pow(index + 1, 3);
+        index += 1;
+    }
+    return arr;
+}
