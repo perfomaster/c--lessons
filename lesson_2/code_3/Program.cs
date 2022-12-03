89e7e68295a64b1fa09399abf0b7ce8302b5aa44
NIKITA LITVINENKO <litvinovnick24@gmail.com>
2022-12-03 19:54:40 +0300

    code 3 is completed

--- lesson_2/code_3/Program.cs
+++ lesson_2/code_3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 15: Напишите программу, 
+которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, 
+является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int number = int.Parse(Console.ReadLine());
+
+if (number > 5 && number < 8) {
+    Console.WriteLine("Weekend");
+} else if (number > 0 && number < 6) {
+    Console.WriteLine("Weekdays");
+}
